af4651e1612034b8828924a3e7e50594326b595c
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 19:49:32 -0400

    KOT-001 - Inicializar proyecto GameZone

---
+++ projectKotlin/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.2.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
